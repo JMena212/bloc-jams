7d10a19b0da2fd75004e6d7022de67fc14735a5b
Jose Mena <josemena0114@gmail.com>
2018-01-25 03:02:22 +0000

    first attempt

--- scripts/album.js
+++ scripts/album.js
@@ -67,13 +67,17 @@ var albumPicasso = {
      }
  };
  
+ 
+ 
+    
+ 
  var findParentByClassName = function(element, targetClass) {
-    if (element) {
-        var currentParent = element.parentElement;
-        while (currentParent.className !== targetClass && currentParent.className !== null) {
-            currentParent = currentParent.parentElement;
+    if (typeof element.parentElement != "undefined") {
+        console.log("no parent found")
         }
-        return currentParent;
+    }
+    else if (typeof element.parentElement != "undefined"){
+        
     }
 };
 
